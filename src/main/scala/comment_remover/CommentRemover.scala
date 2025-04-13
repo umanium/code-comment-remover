2a57dcdeff61446e2a374aa1e4c0653772b9a9dc
Luqman Abdul Mushawwir <abdulmushawwir@gmail.com>
2025-04-13 12:13:33 +0300

    Add comment remover for C-style comments

--- src/main/scala/comment_remover/CommentRemover.scala
+++ src/main/scala/comment_remover/CommentRemover.scala
@@ -2,7 +2,13 @@ package comment_remover
 
 import scala.util.matching.Regex
 
+enum Style:
+  case Python, C
+
 object CommentRemover:
-  def removeComment(s: String): String =
-    val commentRegex: Regex = """#.*""".r
-    commentRegex.replaceAllIn(s, "")
+  def removeComment(s: String, style: Style): String =
+    val regexes: Seq[Regex] = style match
+      case Style.Python => Seq("""#.*\s*""".r)
+      case Style.C => Seq("""/\*[\S\s]*\*/\s*""".r, """//.*\s*""".r)
+
+    regexes.foldLeft(s)((st, regex) => regex.replaceAllIn(st, "")).trim
--- src/test/scala/comment_remover/CommentRemoverTest.scala
+++ src/test/scala/comment_remover/CommentRemoverTest.scala
@@ -3,7 +3,7 @@ package comment_remover
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should
 
-class CommentRemoverTest extends AnyFunSpec with should.Matchers {
+class CommentRemoverTest extends AnyFunSpec with should.Matchers:
   describe("CommentRemover"):
     it("Should remove a python-style comment in a code string"):
       val code: String = """# this is a comment
@@ -11,10 +11,41 @@ class CommentRemoverTest extends AnyFunSpec with should.Matchers {
         |l = [x for x in range(10)] # this is also a comment""".stripMargin
 
       val expectedCode: String =
-        """
-          |
-          |l = [x for x in range(10)] """.stripMargin
+        """l = [x for x in range(10)]""".stripMargin
 
-      val actualCode: String = CommentRemover.removeComment(code)
+      val actualCode: String = CommentRemover.removeComment(code, Style.Python)
       actualCode shouldEqual expectedCode
-}
+
+    it("Should remove a C-style comment in a code string"):
+      val code: String =
+        """/*
+          | * Calculate the factorial of n
+          | */
+          |function factorial(n) {
+          |  // check n is valid
+          |  if (typeof n !== 'number' || !Number.isInteger(n) || n < 0) {
+          |    throw new Error('n must be a non-negative integer');
+          |  }
+          |
+          |  // base case
+          |  if ((n === 1) || (n === 0)) {
+          |    return 1;
+          |  }
+          |  // other cases
+          |  return n * factorial(n - 1)
+          |}""".stripMargin
+
+      val expectedCode: String =
+        """function factorial(n) {
+          |  if (typeof n !== 'number' || !Number.isInteger(n) || n < 0) {
+          |    throw new Error('n must be a non-negative integer');
+          |  }
+          |
+          |  if ((n === 1) || (n === 0)) {
+          |    return 1;
+          |  }
+          |  return n * factorial(n - 1)
+          |}""".stripMargin
+
+      val actualCode: String = CommentRemover.removeComment(code, Style.C)
+      actualCode shouldEqual expectedCode
